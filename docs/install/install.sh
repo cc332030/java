3e9655f0fc9c0b9e7c579f76cd7e49a4aae9c4ff
c332030 <c332030@gmail.com>
2021-07-14 20:14:07 +0800

    feat: install jdk

---
+++ docs/install/install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo please input jdk download url：
+read -r url
+echo "url: ${url}"
+
+newUrl="https://c332030.com/proxy?url=${url}"
+echo "newUrl: ${newUrl}"
+
+curl -L "${url}" -o /tmp/jdk.tar.gz
+
+jdk_home=/usr/local/jdk
+
+rm -rf "${jdk_home}"
+mkdir -p "${jdk_home}"
+tar -zxf /tmp/jdk.tar.gz -C "${jdk_home}"
+
+java_home="${jdk_home}/$(ls ${jdk_home})"
+echo "java_home: ${java_home}"
+
+old_path="export PATH=\""
+new_path="export JAVA_HOME=${java_home}\n${old_path}\$JAVA_HOME/bin;"
+
+sed_command="s@${old_path}@${new_path}@g"
+echo "sed_command: ${sed_command}"
+
+#init_script=~/.bashrc
+init_script=~/.zshrc
+
+sed -i "$sed_command" "${init_script}"
+
+# shellcheck disable=SC1090
+source "${init_script}"
+
+echo successfully
